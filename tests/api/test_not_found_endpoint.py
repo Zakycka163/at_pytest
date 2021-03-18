d0671ed3daee8eef55bf92bd8b4fbc6f3b71426e
Dmitry St <dmitry.st@msp360.com>
2021-03-18 22:31:10 +0300

    Update test_not_found_endpoint.py

--- tests/api/test_not_found_endpoint.py
+++ tests/api/test_not_found_endpoint.py
@@ -2,9 +2,8 @@ import requests
 from random import choice
 
 base_url = "http://localhost:5585"
-method = choice(["PATCH", "OPTIONS", "LOCK", "LINK"])
 
 
 def test_not_found_endpoint():
-    response = requests.request(method, url=base_url + "/device3")
+    response = requests.get(base_url + "/device3")
     assert response.status_code == 404

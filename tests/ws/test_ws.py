137cbb2d194003106cc35a6a3fc61b28666a2933
Dmitry St <dmitry.st@msp360.com>
2021-03-19 10:48:22 +0300

    skip test

--- tests/ws/test_ws.py
+++ tests/ws/test_ws.py
@@ -1,9 +1,11 @@
+import pytest
 import requests
 import asyncio
 import websockets
 
 
-def create_ws_session():
+@pytest.mark.skip(reason="Prepared template - TODO")
+def test_create_ws_session():
     pass
 
 # TODO - create WS session, work with WS
